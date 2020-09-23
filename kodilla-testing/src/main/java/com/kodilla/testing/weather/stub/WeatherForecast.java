8d88bf40c9800d5e30281a155010e5645e584916
Patryk Ostachowicz <ostach98@vp.pl>
2020-09-23 21:52:42 +0200

    6.5 Weather Forecast poprawki

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -5,35 +5,39 @@ import java.util.*;
 public class WeatherForecast {
     private Temperatures temperatures;
 
-    public WeatherForecast(Temperatures temperatures){
+    public WeatherForecast(Temperatures temperatures) {
         this.temperatures = temperatures;
     }
 
-    public Map<String, Double> calculateForecast(){
-        Map<String, Double> resultMap =  new HashMap<>();
+    public Map<String, Double> calculateForecast() {
+        Map<String, Double> resultMap = new HashMap<>();
 
         for (Map.Entry<String, Double> temperature :
-           temperatures.getTemperatures().entrySet()){
+                temperatures.getTemperatures().entrySet()) {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
         return resultMap;
     }
 
-    public Double calculateAverage(){
+    public Double calculateAverage() {
         double sum = 0;
-        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()){
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
             sum += temperature.getValue();
         }
         double avg = sum / temperatures.getTemperatures().size();
         return avg;
     }
 
-    public Double calculateMedian(){
+    public Double calculateMedian() {
         List<Double> temperaturesList = new ArrayList<>(temperatures.getTemperatures().values());
         Collections.sort(temperaturesList);
+        Double median;
         int med = temperaturesList.size() / 2;
-        double median = temperaturesList.get(med);
-        return median;
+        if (temperaturesList.size() % 2 == 1) {
+            median = temperaturesList.get(med);
+        } else {
+            median = (temperaturesList.get(med) + temperaturesList.get(med)) / 2;
+        }
+        return median ;
     }
-
 }
