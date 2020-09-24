a20937a7c9dee2b4dbce345e4bbd11d5db34076c
Patryk Ostachowicz <ostach98@vp.pl>
2020-09-24 19:35:19 +0200

    6.5 poprawka2

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -73,6 +73,55 @@ public class WeatherForecastTestSuite {
         //Then
         Assertions.assertEquals(25.56, averageTemp);
     }
+    @Test
+    void testCalculateAverageWithMock1() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Krakow", 26.2);
+
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double averageTemp = weatherForecast.calculateAverage();
+
+        //Then
+        Assertions.assertEquals(25.85, averageTemp);
+    }
+
+    @Test
+    void testCalculateAverageWithMock2() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Warszawa", 25.2);
+        temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double averageTemp = weatherForecast.calculateAverage();
+
+        //Then
+        Assertions.assertEquals(25.599999999999998, averageTemp);
+    }
+    @Test
+    void testCalculateAverageWithMock3() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Krakow", 26.2);
+        temperaturesMap.put("Wroclaw", 24.8);
+        temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double averageTemp = weatherForecast.calculateAverage();
+
+        //Then
+        Assertions.assertEquals(25.700000000000003, averageTemp);
+    }
 
     @Test
     void testCalculateMedianWithMock() {
@@ -91,5 +140,53 @@ public class WeatherForecastTestSuite {
 
         //Then
         Assertions.assertEquals(25.5, medianTemp);
+    }
+    @Test
+    void testCalculateMedianWithMock1() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Warszawa", 25.2);
+        temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double medianTemp = weatherForecast.calculateMedian();
+
+        //Then
+        Assertions.assertEquals(25.65, medianTemp);
+    }
+    @Test
+    void testCalculateMedianWithMock2() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Krakow", 26.2);
+        temperaturesMap.put("Wroclaw", 24.8);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double medianTemp = weatherForecast.calculateMedian();
+
+        //Then
+        Assertions.assertEquals(25.5, medianTemp);
+    }
+    @Test
+    void testCalculateMedianWithMock3() {
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Krakow", 26.2);
+        temperaturesMap.put("Wroclaw", 24.8);
+        temperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double medianTemp = weatherForecast.calculateMedian();
+
+        //Then
+        Assertions.assertEquals(25.8, medianTemp);
     }
 }
